2377eb5ba718d08fd9d23b5240d066e359a4f81d
Shyamkumar Yadav <shyamkumaryadav2003@gmail.com>
2024-03-10 01:37:04 +0530

    Add timezone support and update date and time formatting

--- mock.v2.py
+++ mock.v2.py
@@ -30,6 +30,7 @@ TEXT = "text"
 
 
 faker = Faker()
+TIME_ZONE = "Asia/Kolkata"
 
 
 def get_uuid():
@@ -40,15 +41,25 @@ def get_instance_id(_uuid):
     return f"uuid:{_uuid}"
 
 
-def format_openrosa_datetime(dt=None):
-    # 02:24:00.000+05:30, 14:24:00.000+05:30 if it's time
-    # 2024-03-09T02:27:00+05:30 if it's datetime
-    dt = dt or datetime.datetime.now(tz=pytz.UTC)
+def format_openrosa_datetime(dt=None, tz=pytz.timezone(TIME_ZONE)):
+    dt = dt or datetime.datetime.now(tz=tz)
     if isinstance(dt, datetime.datetime):
+        # datetime in ISO 8601 format :YYYY-MM-DD(T)HH:MM:SS.mmm+HH:MM
         return dt.isoformat("T", "milliseconds")
     elif isinstance(dt, datetime.time):
-        return dt.isoformat()
+        # time in ISO 8601 format :HH:MM:SS.mmm+HH:MM
+        return (
+            tz.localize(
+                datetime.datetime.combine(
+                    datetime.date.today(),
+                    dt,
+                )
+            )
+            .isoformat("T", "milliseconds")
+            .split("T")[-1]
+        )
     elif isinstance(dt, datetime.date):
+        # date in ISO 8601 format :YYYY-MM-DD
         return dt.isoformat()
     return str(dt)
 
@@ -57,36 +68,38 @@ def get_point():
     def _get_item(s=0, e=1, r=6):
         return round(randint(s, e) * random(), r)
 
-    lat = _get_item(-90, 90)
-    lon = _get_item(-180, 180)
+    print(faker.local_latlng(country_code="IN"))
+    lat, lon = faker.local_latlng(country_code="IN", coords_only=True)
     return f"{lat} {lon} {_get_item(0,10, 1)} {_get_item(0,10, 1)}"
 
 
 def get_random_datetime(_type="datetime"):
-    dt = faker.date_time_between(start_date="-5y", end_date="now")
-    dt = dt.astimezone(pytz.timezone(faker.random_element(pytz.all_timezones)))
-    if _type == "datetime":
+    dt = faker.date_time_between(
+        start_date="-5y", end_date="now", tzinfo=pytz.timezone(TIME_ZONE)
+    )
+    if _type == "dateTime":
         return format_openrosa_datetime(dt)
     if _type == "time":
-        return faker.time(pattern="%H:%M:%S.%f%z")
+        return format_openrosa_datetime(dt.time())
     if _type == "date":
-        return faker.date()
+        return format_openrosa_datetime(dt.date())
+
+
+start_time = format_openrosa_datetime()
 
 
 def get_data(survey):
     result = {}
     for item in survey:
-        name = item.get(NAME) or item.get("$autoname")
+        name = item.get(NAME)
         if name is None:
             continue
 
         data_type = item.get("type")
         appearance = item.get(CONTROL, {}).get(APPEARANCE)
-        current_time = format_openrosa_datetime()
 
         res = item.get("default", "")
         # SELECT QUESTIONS
-        # select one, select all that apply, rank
         if data_type in [SELECT_ALL_THAT_APPLY, SELECT_ONE, RANK]:
             choices = item.get("choices")
             if choices is None:
@@ -113,18 +126,34 @@ def get_data(survey):
                 res = faker.word()
 
         # DATE AND TIME
-        elif data_type in ["datetime", "date", "time"]:
+        elif data_type in ["dateTime", "date", "time"]:
             res = get_random_datetime(data_type)
 
         # META
-        elif data_type in ["start", "end"]:
-            res = current_time
+        elif data_type == "start":
+            res = start_time
+        elif data_type in "end":
+            res = format_openrosa_datetime()
+        elif data_type == "today":
+            res = format_openrosa_datetime(datetime.datetime.now().date())
 
         # NUMBER QUESTIONS
-        elif data_type in ["integer", "range"]:
-            res = randint(0, 99999)
+        elif data_type in "integer":
+            res = faker.pyint(max_value=randint(9999, 9999999))
         elif data_type == "decimal":
-            res = round(random() * randint(0, 99999), randint(1, 10))
+            res = faker.pydecimal(
+                left_digits=randint(1, 5),
+                right_digits=randint(1, 5),
+                positive=True,
+            )
+
+        # RANGE QUESTIONS
+        elif data_type == "range":
+            parameters = item.get("parameters", {})
+            res = faker.random_int(
+                min=int(float(parameters.get("start", 0))),
+                max=int(float(parameters.get("end", 99999))),
+            )
 
         # GEO QUESTIONS
         elif data_type == "geopoint":
@@ -134,7 +163,7 @@ def get_data(survey):
         elif data_type == "geoshape":
             p1 = get_point()
             res = ";".join(
-                [p1] + [get_point() for _ in range(1, randint(2, 10))] + [p1]
+                [p1] + [get_point() for _ in range(1, randint(4, 10))] + [p1]
             )
 
         # GROUP
@@ -171,10 +200,6 @@ def prepare_submission(asset):
             "@xmlns:orx": "http://openrosa.org/xforms",
         }
     )
-    # xmlns:jr="http://openrosa.org/javarosa"
-    # xmlns:orx="http://openrosa.org/xforms"
-    # id="all-question-types"
-    # version="2024022901"
     xml = unparse(
         {
             asset.get(NAME): data,
